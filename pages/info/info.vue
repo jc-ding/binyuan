8f2923efdbf4d6aaf830ff7d681b1a5bdee062df
ooodjc <ooodjc@163.com>
2021-10-18 22:15:44 +0800

    0.04

---
+++ pages/info/info.vue
@@ -0,0 +1,57 @@
+<template>
+	<view>
+		<image src="../../static/logo.png" mode="widthFix" style="width: 100%;"></image>
+		<view class="center2 borderUnderline padding20" style="background-color: #FFFFFF;">
+			<view class="">
+				<view class="center2" style="font-size: 40rpx;">白果爱婴坊</view>
+				<view class="center2" style="margin-top: 20rpx; color: #8a8a8a;">8:00~21:00  999浏览</view>
+			</view>
+		</view>
+		<view class="center padding20" style="background-color: #FFFFFF;">
+			<image src="../../static/map.png" mode="" style="width: 50rpx; height: 50rpx;"></image>
+			<view class="">
+				<view class="">白果镇楚南西路65号</view>
+				<view class="" style="font-size: 22rpx; color: #8a8a8a; margin-top: 20rpx;">距您约 [100.01km]</view>
+			</view>
+			<view class="center" style="margin-left: auto;">
+				<view class="padding20" style="border-right: 1rpx solid #f2f2f2">
+					<image src="../../static/taxi.png" mode="" style="width: 50rpx; height: 50rpx;"></image>
+				</view>
+				<view class="padding20">
+					<image src="../../static/phone.png" mode="" style="width: 50rpx; height: 50rpx;"></image>
+				</view>
+			</view>
+		</view>
+		
+		<view class="" style="background-color: #FFFFFF; margin-top: 40rpx;">
+			<view class="center borderUnderline padding20" style="padding-left: 200rpx; padding-right: 200rpx;">
+				<view class="" style="color: red; border-bottom: 2rpx solid red">介绍</view>
+				<view class="" style="color: #515151; margin-left: auto;">动态</view>
+				<view class="" style="color: #515151; margin-left: auto;">评论</view>
+			</view>
+			<view class="padding20 center2">
+				<image src="../../static/logo.png" mode="widthFix"></image>
+			</view>
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	page{
+		background-color: #F2F2F2;
+	}
+</style>
